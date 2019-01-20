f4eee9bc46075a256c5cd1fc9c7464b259df0f95
Vipan Singh <vksinghh@Vipans-Air.Dlink>
2019-01-20 22:58:40 +0530

    First commit

---
+++ src/main/java/com/example/vksinghhdemo/VksinghhdemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.vksinghhdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VksinghhdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VksinghhdemoApplication.class, args);
+	}
+
+}
+
